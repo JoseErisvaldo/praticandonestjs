99d1c73c84d2aca1e2fae427ad3e22be7a8355c1
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-07-29 00:51:43 -0300

    refactor:  code formatting and readability in establishment module files

--- src/establishment/dto/create-establishment.dto.ts
+++ src/establishment/dto/create-establishment.dto.ts
@@ -1,10 +1,10 @@
 export class CreateEstablishmentDto {
-    name: string;
-    address: string;
-    phoneNumber: string;
-    email: string;
-    website?: string;
-    description?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+  name: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+  website?: string;
+  description?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
